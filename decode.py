efba44ecb6dcfa06b2212fba3d4518d374c80c0f
Kevin Ellis <ellisk@mit.edu>
2015-11-24 11:49:29 -0500

    re- factoring that might have broken something

--- decode.py
+++ decode.py
@@ -7,14 +7,16 @@ def flip():
 
 def guard_expression(d):
     assert d > 0
-    z = integer_expression(d - 1)
+
     c1 = flip()
     c2 = flip()
 
+    z = integer_expression(d - 1)
+
     o = None
-    if c1: o = '= '
-    if not c1 and c2: o = '< '
-    if not c2 and not c2: o = '> '
+    if c1: o = '='
+    if not c1 and c2: o = '>'
+    if not c2 and not c2: o = '<'
 
     return "(%s %s)" % (o,z)
 
@@ -28,41 +30,45 @@ def array_expression(d):
         return "a"
     
     c2 = flip()
+    c3 = None
+    if d > 2: c3 = flip()
 
     lp = array_expression(d - 1)
     z = integer_expression(d - 1)
 
     if d == 2:
-        if c2 and c1: return 'nil'
-        if c2 and not c1: return 'a'
-        if not c2 and c1:
+        if c1 and c2: return 'nil'
+        if c1 and (not c2): return 'a'
+        if (not c1) and c2:
             return '(cdr %s)' % lp
-        if not c2 and not c1:
+        if (not c1) and (not c2):
             return '(list %s)' % z
+        assert False
+    
     assert d > 2
-    c3 = flip()
     g = guard_expression(d - 1)
     if c1 and c2 and c3: return 'nil'
-    if c1 and c2 and not c3: return 'a'
-    if c1 and not c2 and c3: return '(cdr %s)' % lp
-    if c1 and not c2 and not c3: return '(list %s)' % z
-    if not c1 and c2 and c3: return '(filter %s %s)' % (g,lp)
+    if c1 and c2 and (not c3): return 'a'
+    if c1 and (not c2) and c3: return '(cdr %s)' % lp
+    if c1 and (not c2) and (not c3): return '(list %s)' % z
+    if (not c1) and c2 and c3: return '(filter %s %s)' % (g,lp)
 
     assert False
 
 def integer_expression(d):
     assert d > 0
     if d == 1: return '0'
-    zp = integer_expression(d - 1)
-    l = array_expression(d - 1)
 
     c1 = flip()
     c2 = flip()
     c3 = flip()
 
+    zp = integer_expression(d - 1)
+    l = array_expression(d - 1)
+
     if c1 and c2: return '0'
-    if c1 and not c2: return '(+1 %s)' % zp
-    if not c1 and c2: return '(-1 %s)' % zp
+    if c1 and (not c2): return '(+1 %s)' % zp
+    if (not c1) and c2: return '(-1 %s)' % zp
     if c3: return '(car %s)' % l
     return '(length %s)' % l
 
@@ -88,5 +94,6 @@ def main(t):
 
     return '(if (%s %s)\n%s\n(append\n%s\n%s\n%s))' % (g,target,b,x,y,z)
     
-print main([0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1,0,1,0,1,1,0,0,0,1,1,1,1,0,0,0,0,0,1,0,0,0,1,1,1,1,0,0,1,0,0,0,1,0,1,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,0,1,1,0,1,0,0,0,0,0,0,0])
-print main([0,0,0,0,1,1,0,1,1,0,0,0,0,1,0,0,1,0,1,0,1,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,0,0,1,1,0,1,0,1,0,1,0,0,1,0,1,1,0,0,0,0,1,1,0,0,0,1,0,0,1,0,1,0,0,1,0,0,0,1,0,1,1,0,0,0,1,1,1,1,0,0,0,1,0,0,1,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,1,0,0,1,0,0,0,0,1,0,0,0,0,1,1,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0])
+#print main([0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1,0,1,0,1,1,0,0,0,1,1,1,1,0,0,0,0,0,1,0,0,0,1,1,1,1,0,0,1,0,0,0,1,0,1,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,0,1,1,0,1,0,0,0,0,0,0,0])
+#print main([0,0,0,0,1,1,0,1,1,0,0,0,0,1,0,0,1,0,1,0,1,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,0,0,1,1,0,1,0,1,0,1,0,0,1,0,1,1,0,0,0,0,1,1,0,0,0,1,0,0,1,0,1,0,0,1,0,0,0,1,0,1,1,0,0,0,1,1,1,1,0,0,0,1,0,0,1,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,1,0,0,1,0,0,0,0,1,0,0,0,0,1,1,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0])
+print main([0,1,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,0,0,0,1,0,0,0,0,1,0,0,0,1,1,0,1,1,0,1,0,1,0,0,0,1,0,1,1,0,1,1,0,1,0,0,1,0,0,0,0,0,0,0,1,1,1,0,1,1,0,0,0,1,0,0,0,1,0,1,1,1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,1,0,1,1,0,0,0,1,0,1,0,0])
