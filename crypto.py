707870dc76e7c8e9bdeed19bb6e67945b3e1d990
Kevin Ellis <ellisk@mit.edu>
2015-12-17 12:11:29 -0500

    cryptomini interface

---
+++ crypto.py
@@ -0,0 +1,61 @@
+import time
+import random
+import re
+import sys
+
+from pycryptosat import Solver
+
+s = Solver(threads = 3)
+
+
+hs = [] # holes
+clauses = []
+with open(sys.argv[1],'r') as f:
+    for l in f:
+        if len(l) > len('c hole ') and l[:len('c hole ')] == 'c hole ':
+            ms = re.findall(r'(\d+) \- (\d+)', l)[0]
+            ms = range(int(ms[0]),int(ms[1])+1)
+            hs = hs + ms
+        elif len(l) > 0 and not 'c' in l and not 'p' in l:
+            vs = re.findall(r'(\-?\d+)',l)
+            assert vs[-1] == '0'
+            clause = [int(v) for v in vs[:-1] ]
+            clauses.append(clause)
+            s.add_clause(clause)
+print "Loaded",len(clauses),"clauses with",len(hs),"holes"
+subspace_dimension = int(sys.argv[2])
+
+def random_projection():
+    s.add_xor_clause([v for v in hs if random.random() > 0.5 ],random.random() > 0.5)
+
+def try_solving():
+    print "About to run solver ==  ==  ==  > "
+    start_time = time.time()
+    result = s.solve()
+    print "Ran solver in time",(time.time() - start_time)
+    if result[0]:
+        bindings = {}
+        for v in range(len(result[1])):
+            if v in hs:
+                bindings[v] = result[1][v]
+        print "Satisfiable."
+        return bindings
+    else:
+        print "Unsatisfiable."
+        return False
+
+
+def try_sampling():
+    for j in range(subspace_dimension):
+        random_projection()
+    result = try_solving()
+    if result:
+        print "Random projection satisfied"
+        print result
+        s.add_clause([ (-1 if result[h] else 1)*h for h in result ])
+        if try_solving():
+            print "Sample rejected"
+        else:
+            print "Unique so sample accepted"
+            
+try_sampling()
