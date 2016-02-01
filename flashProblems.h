4e1af4909fb14c05c97cf0acd40d8d668728a199
Kevin Ellis <ellisk@mit.edu>
2016-02-01 08:23:47 -0500

    counting data

--- flashProblems.h
+++ flashProblems.h
@@ -1,5 +1,2 @@
 // automatically generated
-testCase("Three <2: vincent> Jeff","(2: vincent)");
-testCase("Don Kyle <3: ricky> virgil","(3: ricky)");
-testCase("herbert is <2: marion> morris","(2: marion)");
-testCase("country music <9: refrigerator>","(9: refrigerator)");
+testCase("My name is John.","John");
--- generalSolver.py
+++ generalSolver.py
@@ -5,6 +5,15 @@ from crypto import ProgramSolver,log2
 from subprocess import Popen, PIPE
 from itertools import permutations
 
+useLowerBound = True
+
+def reverse(l):
+    return list(reversed(l))
+
+def countFirst(l):
+    return len([ x for x in l[1:] if x == l[0] ])
+
+
 
 production_lengths = {}
 def record_production(p,d,l):
@@ -127,22 +136,28 @@ def parse_tape(t):
 
 class GeneralSolver(ProgramSolver):
     def parse_tape(self,t):
-        p,m = parse_tape(t)
-        return p,m
+        if len(t) == 64:
+            p,m = parse_tape(t)
+            return p,m
+        if len(t) == 67:
+            global tape
+            global tape_index
+            tape_index = 0
+            tape = [ f == 1 for f in t ]
+            return integer_expression(5)
+        assert False
 
-def marginal_sort(p,n):
+def marginal_evaluation(p,n,f):
     multiset = {}
     for pp in p:
         multiset[pp] = 1 + multiset.get(pp,0)
-    print len(multiset),"types of programs"
     
-    l = list(range(1,n+1))
     successes = 0
     attempts = 50
     for attempt in range(attempts):
-        lp = list(range(1,n+1))
-        random.shuffle(lp)
-        lp = "(list %s)" % (str(list(lp)).replace(',',' ').replace(']','').replace('[',''))
+        testCase = list(range(1,n+1))
+        random.shuffle(testCase)
+        lp = "(list %s)" % (str(list(testCase)).replace(',',' ').replace(']','').replace('[',''))
         for pp in multiset:
             w = multiset[pp]
             pp = pp.replace('append','safe-append').replace("\n"," ")
@@ -151,19 +166,47 @@ def marginal_sort(p,n):
             if "bottom" in stdout or stderr != "": continue
             try:
                 output = map(int,stdout.replace("(","").replace(")","").split(" "))
-                if output == l: successes += w
+                if output == f(testCase): successes += w
             except:
                 continue
     return float(successes)/len(p)/attempts
 
+def marginal_counting(p,n):
+    multiset = {}
+    for pp in p:
+        multiset[pp] = 1 + multiset.get(pp,0)
+    
+    successes = 0
+    attempts = 50
+    for attempt in range(attempts):
+        (testCase,target) = countTestCases(1,forceL = n)[0]
+        lp = "(list %s)" % (str(testCase).replace(',',' ').replace(']','').replace('[',''))
+        for pp in multiset:
+            w = multiset[pp]
+            po = Popen(["./evaluateGeneral.scm",pp,lp], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            stdout, stderr = po.communicate()
+            if "bottom" in stdout or stderr != "": continue
+            try:
+                output = int(stdout.strip())
+                if output == target: successes += w
+            except:
+                continue
+    return float(successes)/len(p)/attempts
+
+
 def outputTestCases(ts):
     global dumpPrefix
+    enforce = "enforce" if isinstance(ts[0][1],list) else "enforce_counting"
+    testCase= "test_case" if isinstance(ts[0][1],list) else "counting_case"
     with open(dumpPrefix+"/generalTests.sk","w") as f:
-        f.write("include \"general.sk\";\nharness void main() { enforce(); \n")
+        f.write("include \"general.sk\";\nharness void main() { %s(); \n" % enforce)
         for t in range(len(ts)):
             i = ",".join(map(str,ts[t][0]))
-            o = ",".join(map(str,ts[t][1]))
-            l = "test_case({%s},{%s});\n" % (i,o)
+            if isinstance(ts[0][1],list):
+                o = "{" + ",".join(map(str,ts[t][1])) + "}"
+            else:
+                o = str(ts[t][1])
+            l = "%s({%s},%s);\n" % (testCase,i,o)
             f.write(l)
         f.write("}")
 
@@ -189,16 +232,24 @@ def makeTestCases(n,f):
             ts += [t]
     return ts
 
+def countTestCases(n, forceL = None):
+    tests = []
+    while len(tests) < n:
+        if forceL == None:
+            l = random.randint(1,4)
+        else:
+            l = forceL
+        k = random.randint(1,l)
+        target = random.randint(0,7)
 
-            
+        i = [ random.randint(0,7) for j in range(l-k) ] + [target]*k
+        random.shuffle(i)
+        i = [target] + i
+        if countFirst(i) == k and not str((i,k)) in map(str,tests):
+            tests += [(i,k)]
+    return tests
 
-if False:
-    samples = []
-    marginal_sort(samples,2)
-    assert False
 
-def reverse(l):
-    return list(reversed(l))
 
 
 random.seed(os.urandom(10))
@@ -217,36 +268,74 @@ else:
         print m
         print len(m)
         print production_lengths
-    elif 'sort' == sys.argv[1] or 'reverse' == sys.argv[1]:
-        print "%s test cases:" % sys.argv[1]
+    elif 'sort' == sys.argv[1] or 'reverse' == sys.argv[1] or 'count' == sys.argv[1]:
+
+        # = len(sys.argv) == 2 or sys.argv[2] == 'lb'
         
+        print "%s test cases:" % sys.argv[1]
+
+        if sys.argv[1] == "sort":
+            correctImplementation = sorted
+            testCases = makeTestCases(int(sys.argv[2]),sorted)
+            tapeLength = 64
+            MAXLIST = 3
+            MINLENGTH = 23
+        if sys.argv[1] == "count":
+            correctImplementation = countFirst
+            testCases = countTestCases(int(sys.argv[2]))
+            tapeLength = 67
+            MAXLIST = 5
+            MINLENGTH = 3
+        if sys.argv[1] == "reverse":
+            correctImplementation = reverse
+            testCases = makeTestCases(int(sys.argv[2]),reverse)
+            tapeLength = 64
+            MAXLIST = 3
+            MINLENGTH = 23
+
         os.system("mkdir %s" % dumpPrefix)
-        outputTestCases(makeTestCases(int(sys.argv[2]),sorted if sys.argv[1] == "sort" else reverse))
+        outputTestCases(testCases)
         os.system("cat %s/generalTests.sk" % dumpPrefix)
+
         def generateFormula(el,ml):
             command = "sketch %s/generalTests.sk" % dumpPrefix
-            command += " --fe-def EMBEDDINGLENGTH=%d,MINIMUMLENGTH=%d" % (el,ml)
+            command += " --bnd-unroll-amnt %d --bnd-arr1d-size %d --bnd-arr-size %d" % (MAXLIST,MAXLIST,MAXLIST)
+            command += " --fe-def BOUND=%d,EMBEDDINGLENGTH=%d,MINIMUMLENGTH=%d" % (tapeLength,el,ml)
             command += " --beopt:outputSatNamed %s" % dumpPrefix
             os.system(command)
-        generateFormula(64,23)
+            
+        generateFormula(tapeLength,MINLENGTH)
         shortest,L = GeneralSolver(filename = dumpPrefix + "_1.cnf").shortest_program()
         generateFormula(1,L)
+        
         S = GeneralSolver(filename = dumpPrefix + "_1.cnf",fakeAlpha = 0).model_count()
         print "S =",S
-        a = min(int(L + log2(S)),64)
+        a = min(int(L + log2(S)),tapeLength)
+        
         generateFormula(a,L)
-        N = GeneralSolver(filename = dumpPrefix + "_1.cnf").model_count()
-        print "N =",N
+
         lowerBound = 2**(int(a)-L) + S - 1
-        print "Bounded by",lowerBound
+        if useLowerBound:
+            print "|E| bounded by",lowerBound
+            K = int(log2(lowerBound))
+        else:
+            N = GeneralSolver(filename = dumpPrefix + "_1.cnf").model_count()
+            print "N =",N
+            lowerBound = 2**(int(a)-L) + S - 1
+            print "Bounded by",lowerBound
+            K = int(log2(N) - 1)
         print "Generating samples"
-        K = int(log2(lowerBound))
+        
         samples = sum([ GeneralSolver(filename = dumpPrefix + "_1.cnf").enumerate_solutions(K)[1]
                         for j in range(5) ],[])
         print samples
-        print "Got",len(samples),"samples"
+        print "Got",len(samples),"samples with",len(set(samples)),"unique solutions\n[",
         for l in range(1,15):
-            print marginal_sort(samples,l)
+            if sys.argv[1] == 'count':
+                print marginal_counting(samples,l),',',
+            else:
+                print marginal_evaluation(samples,l,correctImplementation),',',
+        print ']'
         os.system("rm %s_1.cnf" % dumpPrefix)
         os.system("rm -r %s" % dumpPrefix)
     else:
